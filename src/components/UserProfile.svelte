aa61f1bfd74e058daa70b0eaf9529b1dbf38c779
Yuganshu <yuganshu219@gmail.com>
2024-11-09 12:59:24 +0530

    fix styling for UserProfile.svelte

--- src/components/UserProfile.svelte
+++ src/components/UserProfile.svelte
@@ -14,11 +14,11 @@
 {#if openMenu}
 	<Menu bind:visibilityState={openMenu}>
 		<div class="flex flex-col gap-4">
-			<div class="flex flex-col gap-1">
-				<div>
+			<div class="flex flex-col">
+				<div class="mb-2">
 					<Avatar CLASS="size-10" />
 				</div>
-				<span class="text-zinc-50 text-sm sm:text-base"
+				<span class="text-zinc-50 text-sm sm:text-base mb-2"
 					>{userStore.value?.name}
 				</span>
 				<span class="text-zinc-50 text-sm sm:text-base"
@@ -28,7 +28,7 @@
 			<SignOut>
 				<div
 					slot="submitButton"
-					class="w-[230px] rounded-md border-2 border-green-600 text-green-600 px-2 py-1 hover:border-green-500 hover:text-green-500"
+					class="w-[232px] rounded-md border-2 border-green-600 text-green-600 px-2 py-1 hover:border-green-500 hover:text-green-500"
 				>
 					Log Out
 				</div>
