5601a07c67e5828bef51454dad74f17ec5ef9564
Yuganshu <yuganshu219@gmail.com>
2024-11-12 17:49:42 +0530

    modify chat names to display only first 20 chars

--- src/features/chat/components/Chat.svelte
+++ src/features/chat/components/Chat.svelte
@@ -16,7 +16,9 @@
 <div class="flex items-center p-2 bg-zinc-800 rounded-md">
 	<div class="flex-1 flex items-center">
 		<button onclick={() => goto(`/c/${chatId}`)} class="flex-1 text-start">
-			<span class="text-gray-50 text-sm sm:text-base">{chatName}</span>
+			<span class="text-gray-50 text-sm sm:text-base">
+				{chatName.length <= 20 ? chatName : chatName.slice(0, 20) + "..."}
+			</span>
 		</button>
 	</div>
 	<div class="grid grid-cols-2 gap-1">
