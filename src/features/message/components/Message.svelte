5b20bb82faae5c8118bd0d8d191a7a396698adbe
Yuganshu <yuganshu219@gmail.com>
2024-11-12 13:50:21 +0530

    stretch message content to full width

--- src/features/message/components/Message.svelte
+++ src/features/message/components/Message.svelte
@@ -6,26 +6,28 @@
 		role: string;
 		children: Snippet;
 	}
-	
+
 	let { role, children }: Props = $props();
 </script>
 
 {#snippet message(role: string)}
-<div class="p-3 rounded-md {role === 'user' ? 'bg-zinc-700': ''}">
-	<div class="message-grid">
-		<div class="flex flex-col items-center justify-start">
-			{#if role === "user"}
-				<Avatar />
-			{:else}
-				<i class="ri-openai-fill text-green-600 text-3xl"></i>
-			{/if}
-		</div>
-		<!-- content -->
-		<div class="flex flex-col items-start prose prose-invert max-w-full prose-sm sm:prose-base overflow-x-auto text-zinc-200">
-			{@render children()}
+	<div class="p-3 rounded-md {role === 'user' ? 'bg-zinc-700' : ''}">
+		<div class="message-grid">
+			<div class="flex flex-col items-center justify-start">
+				{#if role === "user"}
+					<Avatar />
+				{:else}
+					<i class="ri-openai-fill text-green-600 text-3xl"></i>
+				{/if}
+			</div>
+			<!-- content -->
+			<div
+				class="prose prose-invert max-w-full prose-sm sm:prose-base overflow-x-auto text-zinc-200"
+			>
+				{@render children()}
+			</div>
 		</div>
 	</div>
-</div>
 {/snippet}
 
 {@render message(role)}
