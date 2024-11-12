fce34df9a9a61666e5490ced2d53d5623a42b70e
Yuganshu <yuganshu219@gmail.com>
2024-11-12 13:23:35 +0530

    add favicon and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import '../app.css';
-	import 'remixicon/fonts/remixicon.css';
-	import type { LayoutData } from './$types';
-	import type { Snippet } from 'svelte';
-	import LoginPage from '$components/LoginPage.svelte';
+	import "../app.css";
+	import "remixicon/fonts/remixicon.css";
+	import type { LayoutData } from "./$types";
+	import type { Snippet } from "svelte";
+	import LoginPage from "$components/LoginPage.svelte";
 	import Sidebar from "$components/Sidebar.svelte";
 	import SidebarToggleButton from "$components/SidebarToggleButton.svelte";
-	import { userStore } from '$stores/user.svelte';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
-	import { setupConvex, useQuery } from 'convex-svelte';
+	import { userStore } from "$stores/user.svelte";
+	import { PUBLIC_CONVEX_URL } from "$env/static/public";
+	import { setupConvex, useQuery } from "convex-svelte";
 	import { useConvexClient } from "convex-svelte";
-	import { api } from '$convex/_generated/api';
-	import { dbAddUser } from '$features/auth/db';
-	import { convexClientStore } from '$stores/convexClient.svelte';
+	import { api } from "$convex/_generated/api";
+	import { dbAddUser } from "$features/auth/db";
+	import { convexClientStore } from "$stores/convexClient.svelte";
 
 	interface Props {
 		data: LayoutData;
@@ -23,15 +23,17 @@
 	userStore.value = data.session?.user ?? null;
 	setupConvex(PUBLIC_CONVEX_URL);
 	convexClientStore.value = useConvexClient();
-	const dbGetUserQuery = useQuery(api.users.getUser, {email: userStore.value?.email ?? ""});
-	
+	const dbGetUserQuery = useQuery(api.users.getUser, {
+		email: userStore.value?.email ?? "",
+	});
+
 	$effect(() => {
 		if (dbGetUserQuery.isLoading) return;
 		if (dbGetUserQuery.error) {
-				console.error("error fetching user from db");
-				console.log(dbGetUserQuery.error);
-				return;
-			}
+			console.error("error fetching user from db");
+			console.log(dbGetUserQuery.error);
+			return;
+		}
 		if (dbGetUserQuery.data && dbGetUserQuery.data.length === 0) {
 			if (userStore.value?.name && userStore.value.email) {
 				console.log("adding user to db...");
@@ -44,8 +46,11 @@
 		}
 		console.log(dbGetUserQuery.data);
 	});
-	
 </script>
+
+<svelte:head>
+	<title>ChatGPT Clone V2</title>
+</svelte:head>
 
 <main class="min-h-[100dvh] bg-zinc-800">
 	{#if data.session}
