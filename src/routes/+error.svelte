1472365bc56a088981e6353e0eae54e9b2ed21e4
Yuganshu <yuganshu219@gmail.com>
2024-11-13 10:38:40 +0530

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+<section
+	class="container mx-auto min-h-dvh grid place-content-center text-center gap-4 text-yellow-400"
+>
+	<div class="flex flex-col gap-1 sm:gap-3">
+		{#if $page.status === 404}
+			<i class="ri-robot-2-fill text-7xl sm:text-9xl"></i>
+		{:else}
+			<i class="ri-error-warning-fill text-7xl sm:text-9xl"></i>
+		{/if}
+		<h1 class="text-3xl sm:text-5xl font-bold">{$page.status}</h1>
+		<p class="text-xl sm:text-3xl">{$page.error?.message}</p>
+	</div>
+</section>
